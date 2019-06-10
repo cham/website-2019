ce1e4cf1a74f27d315ea6e2e1cdbda82e5fd2dfa
Dan Neame <danneame@gmail.com>
2019-06-10 15:50:29 +0100

    Remove torus and replace with tree model

--- js/torus.js
+++ js/torus.js
@@ -13,37 +13,46 @@ var scene = new THREE.Scene()
 var camera = new THREE.PerspectiveCamera(40, wSize.width / wSize.height)
 var renderer = new THREE.WebGLRenderer({ alpha: true })
 
-renderer.setClearColor(0x000000, 0)
-renderer.setSize(wSize.width, wSize.height)
+const spotlight = new THREE.SpotLight(0xEECCAA, 1)
+spotlight.position.set(0, 3000, 2000)
 
-camera.position.set(0, 50, 0)
+scene.add(spotlight)
+
+renderer.setClearColor(0x000000, 0)
+renderer.setSize(wSize.width - 20, wSize.height)
+
+camera.position.set(0, 20, 0)
 document.body.appendChild(renderer.domElement)
 
 window.onorientationchange = function () {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
 
-  renderer.setSize( window.innerWidth, window.innerHeight )
+  renderer.setSize( window.innerWidth - 20, window.innerHeight )
 }
 
 window.onresize = function(){
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
 
-  renderer.setSize( window.innerWidth, window.innerHeight )
+  renderer.setSize( window.innerWidth - 20, window.innerHeight )
 }
 
-var cameraRotation = 0
+let cameraRotation = 0
+let lastRotation = cameraRotation
 function tick(){
     requestAnimationFrame(tick)
 
+    if (lastRotation === cameraRotation) {
+      return
+    }
     camera.position.x = Math.sin(cameraRotation) * 100
     camera.position.z = Math.cos(cameraRotation) * 100
     camera.lookAt(new THREE.Vector3(0, 0, 0))
 
     renderer.render(scene, camera)
-
-    cameraRotation += 0.01
+    lastRotation = cameraRotation
+    console.log('rendered')
 }
 tick()
 
@@ -57,21 +66,39 @@ const mmo = (g, m) => {
   return group
 }
 
-const torusknot = () => {
-  var geometry = new THREE.TorusKnotGeometry(20, 5, 200, 16, randInt(6), randInt(6))
-  var material = new THREE.MeshBasicMaterial({
-    color: 0xdddddd
-  })
-  var wireframeMaterial  = new THREE.MeshBasicMaterial({
-    color: 0xffffff,
-    wireframe: true
-  })
-  return mmo(geometry, [
-    material,
-    wireframeMaterial
-  ])
+var material = new THREE.MeshPhongMaterial({
+  color: 0x112233
+})
+var wireframeMaterial  = new THREE.MeshBasicMaterial({
+  color: 0x666699,
+  wireframe: true
+})
+
+const loader = new THREE.OBJLoader()
+const addObj = (path, scale, position, materials) => {
+  loader.load(
+    path,
+    (obj) => {
+      const g = new THREE.Group()
+      obj.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          g.add(mmo(child.geometry, materials))
+        }
+      })
+      g.scale.set(scale, scale, scale)
+      g.position.copy(position)
+      scene.add(g)
+      lastRotation = Infinity
+    }
+  )
 }
 
-scene.add(torusknot())
+addObj('../models/tree.obj', 2.3, new THREE.Vector3(0, -30, 0), [material])
 
 renderer.domElement.className = 'torus-demo'
+
+const setCameraRotation = () => {
+  requestAnimationFrame(setCameraRotation)
+  cameraRotation = -window.scrollY / 300
+}
+requestAnimationFrame(setCameraRotation)
