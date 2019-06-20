79b446251a4a9ba30d34b148e7228006d8f7f48b
Dan Neame <danneame@gmail.com>
2019-06-20 12:33:04 +0100

    Refactor addstats.js

--- js/addstats.js
+++ js/addstats.js
@@ -1,17 +1,11 @@
-var stats = new Stats();
-stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild( stats.dom );
+var stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
 
-function animate() {
-
-	stats.begin();
-
-	// monitored code goes here
-
-	stats.end();
-
-	requestAnimationFrame( animate );
-
+function updateStats() {
+	stats.begin()
+	stats.end()
+	requestAnimationFrame(updateStats)
 }
 
-requestAnimationFrame( animate );
+requestAnimationFrame(updateStats)
